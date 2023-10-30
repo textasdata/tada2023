aac31c160ca77f455f865ba11329dba1b4f3fab4
Brendan O'Connor <brenocon@gmail.com>
2023-10-30 11:19:39 -0400

    program update

--- make.sh
+++ make.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
+
+# First do
+# git clone https://git.overleaf.com/6529ac3d6ece88dceaa1edc8  program_overleaf
+
 set -x
 
 (cd program_overleaf && git pull && pdflatex main)
-cp program_overleaf/main.pdf program.pdf
+cp program_overleaf/main.pdf assets/program.pdf
+git commit -am "program update"
